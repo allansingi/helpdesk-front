5c88a23b73490eda786df75fdbe772cbbbc89b29
allansingi <allansingi@gmail.com>
2023-05-02 15:16:37 +0100

    sidenav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: NavComponent
+    path: '', component: NavComponent
   }
 ];
 
